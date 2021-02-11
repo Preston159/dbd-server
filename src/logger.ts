b832508c1312ec7488c7052960be4a50579b8f0d
Preston Petrie <preston@ppetrie.com>
2021-02-11 12:59:34 -0600

    Moved logger settings to config file

--- src/logger.ts
+++ src/logger.ts
@@ -3,6 +3,7 @@ import type { LoggerOptions, FileParts, RequestType } from './types/types'
 import type { Request } from 'express'
 import * as fs from 'fs'
 import type { WriteStream } from 'fs'
+import HJSON from 'hjson'
 import * as path from 'path'
 
 import { getDateString, API_PREFIX, formatString, padHexByte, padString, removeToken } from './util.js'
@@ -10,12 +11,7 @@ import { getDateString, API_PREFIX, formatString, padHexByte, padString, removeT
 const FNAME_REGEX = /^(?<name>[A-Za-z0-9\-_\.]+)\.(?<ext>[A-Za-z]{1,4})$/
 const DIR_REGEX = /^[A-Za-z0-9\-_]+$/
 
-const opts: LoggerOptions = {
-    console: true,
-    file: true,
-    logDir: 'logs',
-    fileName: 'server.log',
-}
+const opts = HJSON.parse(fs.readFileSync(path.join('.', 'settings', 'logging.hjson')).toString()) as LoggerOptions
 
 let writeStream: WriteStream = null
 let filePath: string = path.join('.', opts.logDir, opts.fileName)
